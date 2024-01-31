09dc9db3572925a91a740466cb5b2266f5a2092c
Adam Mazouz <adam.mazouz.93@gmail.com>
2024-01-31 14:48:21 +0100

    First commit

---
+++ Deploy-CBS-Greenfield/main.tf
@@ -0,0 +1,124 @@
+terraform {
+  required_providers {
+    cbs = {
+      source  = "PureStorage-OpenConnect/cbs"
+      version = "~> 0.9.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+ /*    If Authintication via Service Principles would be used: 
+    1. Uncomment the required params from main.tf and variables.tf 
+    2. Add your service principle in terraform.tfvars              */
+
+provider "azurerm" {
+  features {}
+  # client_id       = var.azure_client_id
+  # client_secret   = var.azure_client_secret
+  # tenant_id       = var.azure_tenant_id
+  # subscription_id = var.azure_subscription_id
+}
+
+provider "cbs" {
+  azure {
+    # client_id       = var.azure_client_id
+    # client_secret   = var.azure_client_secret
+    # tenant_id       = var.azure_tenant_id
+    # subscription_id = var.azure_subscription_id
+  }
+}
+
+resource "azurerm_resource_group" "azure_rg" {
+  name     = format("%s%s", var.resource_group_name, var.resource_group_location)
+  location = var.resource_group_location
+  tags     = var.tags
+}
+
+
+module "CBS_vNET" {
+  source                  = "../Modules/CBS-VNet"
+  resource_group_name     = azurerm_resource_group.azure_rg.name
+  resource_group_location = var.resource_group_location
+  tags                    = var.tags
+}
+
+module "CBS-NAT-GW" {
+  source                  = "../Modules/CBS-NAT-GW"
+  resource_group_name     = azurerm_resource_group.azure_rg.name
+  resource_group_location = var.resource_group_location
+  cbs_system_subnet_id    = module.CBS_vNET.azure_subnet_id.cbs_subnet_sys
+  tags                    = var.tags
+}
+
+module "VM-JUMPBOX" {
+  source                  = "../Modules/VM-JUMPBOX"
+  resource_group_name     = azurerm_resource_group.azure_rg.name
+  resource_group_location = var.resource_group_location
+  tags                    = var.tags
+  cbs_vnet_name           = module.CBS_vNET.cbs_vnet_name
+  cbs_subnet_vms_address  = var.cbs_subnet_vms_address
+  azure_vm_size           = var.azure_vm_size
+  azure_vm_username       = var.azure_vm_username
+  azure_vm_password       = var.azure_vm_password
+}
+
+module "CBS-Key-Vault" {
+  source                  = "../Modules/CBS-Key-Vault"
+  resource_group_name     = azurerm_resource_group.azure_rg.name
+  resource_group_location = var.resource_group_location
+}
+
+module "CBS-Identity" {
+  source                  = "../Modules/CBS-Identity"
+  resource_group_name     = azurerm_resource_group.azure_rg.name
+  resource_group_location = var.resource_group_location
+  cbs_vnet_id             = module.CBS_vNET.cbs_vnet_id
+  depends_on = [ azurerm_resource_group.azure_rg ]
+}
+
+module "CBS-VNET-Peering" {
+  source                         = "../Modules/CBS-VNet-Peering"
+  resource_group_name            = azurerm_resource_group.azure_rg.name
+  cbs_vnet_id                    = module.CBS_vNET.cbs_vnet_id
+  cbs_vnet_name                  = module.CBS_vNET.cbs_vnet_name
+  azure_virtualnetwork_peer_name = var.azure_virtualnetwork_peer_name
+  azure_virtualnetwork_peer_rg   = var.azure_virtualnetwork_peer_rg
+}
+
+module "CBS-Array" {
+  source                  = "../Modules/CBS-Array"
+  array_name              = var.array_name
+  resource_group_name     = azurerm_resource_group.azure_rg.name
+  resource_group_location = var.resource_group_location
+  cbs_vnet_id             = module.CBS_vNET.cbs_vnet_id
+  cbs_subnet_mgmt_name    = module.CBS_vNET.azure_subnet_name.cbs_subnet_mgmt
+  cbs_subnet_iscsi_name   = module.CBS_vNET.azure_subnet_name.cbs_subnet_iscsi
+  cbs_subnet_repl_name    = module.CBS_vNET.azure_subnet_name.cbs_subnet_repl
+  cbs_subnet_sys_name     = module.CBS_vNET.azure_subnet_name.cbs_subnet_sys
+  license_key             = var.license_key
+  cbs_key_vault           = module.CBS-Key-Vault.cbs_key_vault_id
+  log_sender_domain       = var.log_sender_domain
+  alert_recipients        = var.alert_recipients
+  array_model             = var.array_model
+  zone                    = var.zone
+  key_file_path           = var.key_file_path
+  jit_group_ids           = var.jit_group_ids
+  tags                    = var.tags
+  user_assigned_identity  = module.CBS-Identity.user_assigned_identity_id
+  depends_on = [ module.CBS-Identity ]
+
+}
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # cbs-deploy-terraform
+
+
+Terraform examples for deploying Pure Cloud Block Store (CBS) with all the associated pre-requisites
+
+_Note: This repo only supports deploying Cloud Block Store in Azure_
+
+# How to Use
+
+1. Deploy an Azure VM in the same subscription where you are planning to deploy CBS.
+2. SSH to the VM, then install Terraform.
+3. Authenticate to Azure.
+4. Clone this repo.
+5. Navigate to the desired example, and edit `terraform.tfvars` with your own parameters and input. ( `terraform.tfvars` file has a basic example on the required input required for the deployment )
+6. Run `terraform init`, then `terraform apply`
+
+#\_\_\_
+
+# How it is Built
+
+The content of this repo is mostly Azure and CBS Terraform Provider Code. It is split into:
+
+1. Example dir: `Depoly-*` where you would need to navigate and excite the terraform commands.
+
+   | Example Name                  | Cloud Provider | Modules Used                              |
+   | ----------------------------- | -------------- | ----------------------------------------- |
+   | [Deploy-CBS-Prerequisites-Only](/Deploy-CBS-Prerequisites-Only) | Azure          | CBS-VNET,CBS-NAT-GW, VM-JUMPBOX           |
+   | [Deploy-CBS-Array-with-all-Prerequisites](/Deploy-CBS-Greenfield)         | Azure          | CBS-Array,CBS-VNET,CBS-NAT-GW, VM-JUMPBOX |
+
+
+2. Modules files:
+   | Module Name | |
+   | ----------- | --- |
+   | [CBS-VNET](/Modules/CBS-VNet) | Creates an Azure VNet with four Subnet |
+   | [CBS-NAT-GW](/Modules/CBS-NAT-GW) | Creates an Azure NAT GW and associate it with CBS System Subnet |
+   | [CBS-Key-Vault](/Modules/CBS-Key-Vault) | Creates an Azure KeyValut that is used by CBS Terraform Provider to perform management operations |
+   | [CBS-Identity](/Modules/CBS-Identity) | Creates an Azure Custom Role and Assign it to a User Managed Identity |
+   | [VM-JUMPBOX](/Modules/VM-JUMPBOX) | Creates an Azure VM on the same CBS VNet, it can be used as a Jump Host or repurpose as an storage initiator|
+   | [CBS-Array](/Modules/CBS-Array) | Creates a Cloud Block Store Array |
+   | [CBS-VNET-Peering](/Modules/CBS-VNet-Peering) | Creates a peering link between the Terraform VM to the new created CBS VNET | 
+
